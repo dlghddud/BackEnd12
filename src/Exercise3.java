c5dc6f3c0bb9e22ce9df945d1e5aa3241d318e1f
이홍ã영 <hyhy7012@naver.com>
2025-03-31 15:25:37 +0900

    branch file

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Exercise3 {
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수를 입력하세요 : ");
+        int num = sc.nextInt();
+        System.out.println((num/10+(num%10==0?0:1))*10-num);
+    }
 }
