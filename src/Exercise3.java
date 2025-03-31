416bfaadd42e6d9cd8415b208a36cd78b33de525
이홍ã영 <hyhy7012@naver.com>
2025-03-31 16:10:32 +0900

    Second commit

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -6,5 +6,10 @@ public class Exercise3 {
         System.out.print("정수를 입력하세요 : ");
         int num = sc.nextInt();
         System.out.println((num/10+(num%10==0?0:1))*10-num);
+
+        int fehrenheit = 100;
+        float celsius = (int)(5/9f*(fehrenheit - 32)*100 + 0.5)/100f;
+        System.out.println(fehrenheit);
+        System.out.println(celsius);
     }
 }
