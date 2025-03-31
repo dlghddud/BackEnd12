ff52e96f2825185ca8e85adf12b95f8d6b64008e
이홍ã영 <hyhy7012@naver.com>
2025-03-31 15:31:48 +0900

    Second commit

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -6,5 +6,12 @@ public class Exercise3 {
         System.out.print("정수를 입력하세요 : ");
         int num = sc.nextInt();
         System.out.println((num/10+(num%10==0?0:1))*10-num);
+        System.out.println(num);
+        System.out.println(num);
+        System.out.println(num);
+        System.out.println(num);
+        System.out.println(num);
+        System.out.println(num);
+        System.out.println(num);
     }
 }
